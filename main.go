217549a3ad9252704c8a59fcbd2c4801b05ba5b5
meep-morp <gracieaerogirl@gmail.com>
2021-02-08 21:28:03 -0600

    Basic hello world route

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+// func main() {
+// 	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Hello, world!"))
+// 	})
+// 	http.ListenAndServe(":8080", nil)
+// }
+
 func main() {
-	
+
 }
