8c78c123ea579602ed9d3dfbd50d396c8190ea73
meep-morp <gracieaerogirl@gmail.com>
2021-02-08 22:40:05 -0600

    get reqs

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Hello World example
 // func main() {
 // 	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
 // 		w.Write([]byte("Hello, world!"))
@@ -7,6 +15,71 @@ package main
 // 	http.ListenAndServe(":8080", nil)
 // }
 
-func main() {
+// STRUCTS
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
 
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// init slice -- an array with a variable length
+var books []Book
+
+// FUNCTIONS
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop through books and find corresponding ID
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// MAIN
+func main() {
+	// Init Router
+	r := mux.NewRouter()
+
+	// Init DATA
+	books = append(books, Book{ID: "1", Isbn: "44875", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "44876", Title: "Book Two", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "3", Isbn: "44877", Title: "Book Three", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+
+	// Route Handlers
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/book/{id}", deleteBook).Methods("DELETE")
+
+	http.ListenAndServe(":8000", r)
 }
